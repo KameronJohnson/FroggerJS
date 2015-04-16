5c95370988f4d778610f1abfb844f2950169fe27
Kameron <KameronJohnson@gmail.com>
2015-04-15 19:41:55 -0700

    implement Player and Enemy classes

--- js/app.js
+++ js/app.js
@@ -1,39 +1,87 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-}
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-}
-
 // Draw the enemy on the screen, required method for game
+
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player reset
+
+Object.prototype.reset = function() {
+
+  player.x = 200;
+  player.y = 400;
+
+}
+
+// Enemies our player must avoid
+var Enemy = function(x, y) {
+
+    // The image/sprite for our enemies, this uses
+
+    this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = Math.floor(Math.random() * 200) + 100);
+}
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
 
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    if (this.x <= 550) {
+      this.x =+ this.speed * dt;
+    }
+    else {
+      this.x = -2;
+    }
+
+    //If player comes within 25px of an enemy, reset game
+    if (player.x >= this.x - 25 && player.x <= this.x + 25) {
+      if (player.y >= this.y - 25 && player.y <= this.y + 25) {
+        this.reset();
+      }
+    }
+}
+
+Player.prototype.update = function() {
+  //move left
+  if (this.ctlKey === "left" && this.x > 0) {
+      this.x = this.x - 50;
+
+  //move right
+  } else if (this.ctlKey === "right" && this.x !== 400) {
+      this.x = this.x + 50;
+
+  //move up
+  } else if (this.ctlKey === "up") {
+      this.y = this.y - 50;
+
+  //move down
+  } else if (this.ctlKey === "down" && this.y !== 400) {
+      this.y = this.y + 50;
+  }
+      this.ctlKey = null;
+
+      // reset on water
+      if (this.y < 25) {
+        this.reset();
+      }
+}
+
+//input handler
+Player.prototype.handleInput = function(e) {
+  this.ctlKey = e;
+}
+
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
